5f2cfd1b7003820cb03ac638e5064450500e1f89
Your Name <you@example.com>
2022-11-25 04:25:52 -0500

    4loop script created

---
+++ 4loop.sh
@@ -0,0 +1,8 @@
+
+
+#USER MANAGEMENT COMMANDS
+	passwords for user Sandra = adminS31
+					   Dandiel = adminD35
+
+	Personal Access Token for Git and Github login
+	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
