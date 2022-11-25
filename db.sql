7ad4a60a404580892ff079dbfdb476fe2e0755d5
Sandra Odinta <78628198+06Sandra@users.noreply.github.com>
2022-11-25 04:09:46 -0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,3 +1,10 @@
 Database server in progress
 db-password=$password
 db-username=$user
+
+monitor_servers.sh  
+    dataBase_backup.sh 
+    deploy_app.sh 
+    access_account.sh  
+    user_management.sh  
+    file_management.sh 
