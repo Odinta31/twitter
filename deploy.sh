aaabf42f006ec6e46c8c01cd5ed9ba57bb677aaa
Your Name <you@example.com>
2022-11-29 14:01:37 -0500

    deployment to staging environment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 
 #this is a deployment script
 
+
 #monitor_servers.sh  
     dataBase_backup.sh 
     deploy_app.sh 
     access_account.sh  
     user_management.sh  
-    file_management.sh 
+    file_management.sh
+deploy to staging 
