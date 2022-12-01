18e23a92b3b9ec6cd2545f98240e975e4cc5eab2
Sandra Odinta <78628198+06Sandra@users.noreply.github.com>
2022-12-01 02:29:40 -0500

    car servicing feature added

--- app.java
+++ app.java
@@ -5,3 +5,5 @@ please do not hard cord.
 make use of variables and dynamic varibles.
 author: Sandra Odinta
 This is a front end application.
+print("car services automated")
+print("hello $user your car need servicing")
