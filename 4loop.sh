a4385d7840c2b1f0beb3c5e35c5493f3ba4dee54
Sandra Odinta <78628198+06Sandra@users.noreply.github.com>
2022-12-01 12:12:15 -0500

    Update 4loop.sh
    Added comments to the 4loop script

--- 4loop.sh
+++ 4loop.sh
@@ -1,8 +1,13 @@
 
 
 #USER MANAGEMENT COMMANDS
-	passwords for user Sandra = adminS31
-					   Dandiel = adminD35
+	passwords for user 	Sandra = adminS31
+				Dandiel = adminD35
 
-	Personal Access Token for Git and Github login
-	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
+Personal Access Token for Git and Github login
+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
+	
+this is code for a web application.
+lets us give it a try.
+please do not hard cord. 
+make use of variables and dynamic varibles.
